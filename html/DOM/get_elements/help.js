479968d1b01925fe308a2a1c6ea1ffe321da359e
riiinnana <Kirashira@mail.ru>
2025-10-14 23:44:37 +0300

    Delete html directory

--- html/DOM/get_elements/help.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByTagName("span")[0].innerText
-'Борисанова'
-document.getElementsByTagName("span")[1].innerText
-'Арина'
-document.getElementsByTagName("span")[2].innerText
-'2005'
