95c98aab44f3d1f8e11f13c9880028130e7fb0c6
riiinnana <Kirashira@mail.ru>
2025-10-14 23:08:25 +0300

    Add files via upload

---
+++ html/DOM/get_elements/help.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("span")[0].innerText
+'Борисанова'
+document.getElementsByTagName("span")[1].innerText
+'Арина'
+document.getElementsByTagName("span")[2].innerText
+'2005'
